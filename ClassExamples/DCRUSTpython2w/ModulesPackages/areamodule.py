f4864fe7b3315503eaa8853716d920beea4056cc
sarwansingh <sarwan_singh@yahoo.com>
2020-09-04 11:57:20 +0530

    update

---
+++ ClassExamples/DCRUSTpython2w/ModulesPackages/areamodule.py
@@ -0,0 +1,5 @@
+def areaSquare(side):
+  return side * side
+
+def areaTriangle(base, height):
+  return .5 * base * height
---
+++ ClassExamples/DCRUSTpython2w/ModulesPackages/mypack/perimeter.py
@@ -0,0 +1,5 @@
+def perimeterRectangle(length, bredth):
+  return 2*(length + bredth)
+
+def perimeterSquare(side):
+  return 4*side
